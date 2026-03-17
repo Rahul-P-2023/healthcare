0dad90884dd31f96529e006f465bccaf881bd2c4
Rahul Pagare <pagarerahul0110@gmail.com>
2026-03-17 14:06:58 +0530

    new8

--- airflow/bg_job.py
+++ airflow/bg_job.py
@@ -4,32 +4,17 @@ from datetime import timedelta
 from airflow.utils.dates import days_ago
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 
-# Define constants
 PROJECT_ID = "project-b369d3e9-b19b-4338-90a"
 LOCATION = "US"
-SQL_FILE_PATH_1 = "/home/airflow/gcs/data/bigquery/bronze.sql"
-SQL_FILE_PATH_2 = "/home/airflow/gcs/data/bigquery/silver.sql"
-SQL_FILE_PATH_3 = "/home/airflow/gcs/data/bigquery/gold.sql"
 
-# Read SQL query from file
-def read_sql_file(file_path):
-    with open(file_path, "r") as file:
-        return file.read()
-
-BRONZE_QUERY = read_sql_file(SQL_FILE_PATH_1)
-SILVER_QUERY = read_sql_file(SQL_FILE_PATH_2)
-GOLD_QUERY = read_sql_file(SQL_FILE_PATH_3)
-
-# Define default arguments
 ARGS = {
     "owner": "airflow",
-    "start_date": None,
+    "start_date": days_ago(1),
     "depends_on_past": False,
     "retries": 1,
     "retry_delay": timedelta(minutes=5)
 }
 
-# Define the DAG
 with DAG(
     dag_id="bigquery_dag",
     schedule_interval=None,
@@ -42,33 +27,39 @@ with DAG(
         task_id="bronze_tables",
         configuration={
             "query": {
-                "query": BRONZE_QUERY,
+                "query": "{% include 'data/bigquery/bronze.sql' %}",
                 "useLegacySql": False,
-                "priority": "BATCH",
+                "priority": "BATCH"
             }
         },
+        location=LOCATION,
+        project_id=PROJECT_ID
     )
 
     silver_tables = BigQueryInsertJobOperator(
         task_id="silver_tables",
         configuration={
             "query": {
-                "query": SILVER_QUERY,
+                "query": "{% include 'data/bigquery/silver.sql' %}",
                 "useLegacySql": False,
-                "priority": "BATCH",
+                "priority": "BATCH"
             }
         },
+        location=LOCATION,
+        project_id=PROJECT_ID
     )
 
     gold_tables = BigQueryInsertJobOperator(
         task_id="gold_tables",
         configuration={
             "query": {
-                "query": GOLD_QUERY,
+                "query": "{% include 'data/bigquery/gold.sql' %}",
                 "useLegacySql": False,
-                "priority": "BATCH",
+                "priority": "BATCH"
             }
         },
+        location=LOCATION,
+        project_id=PROJECT_ID
     )
 
 bronze_tables >> silver_tables >> gold_tables
