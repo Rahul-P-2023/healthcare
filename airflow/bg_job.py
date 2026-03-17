5d7859bb438b7a01392c56551fe5d687a34ab8e7
Rahul Pagare <pagarerahul0110@gmail.com>
2026-03-17 13:43:20 +0530

    new7

--- airflow/bg_job.py
+++ airflow/bg_job.py
@@ -7,9 +7,9 @@ from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobO
 # Define constants
 PROJECT_ID = "project-b369d3e9-b19b-4338-90a"
 LOCATION = "US"
-SQL_FILE_PATH_1 = "/opt/airflow/dags/sql/bronze.sql"
-SQL_FILE_PATH_2 = "/opt/airflow/dags/sql/silver.sql"
-SQL_FILE_PATH_3 = "/opt/airflow/dags/sql/gold.sql"
+SQL_FILE_PATH_1 = "/home/airflow/gcs/data/bigquery/bronze.sql"
+SQL_FILE_PATH_2 = "/home/airflow/gcs/data/bigquery/silver.sql"
+SQL_FILE_PATH_3 = "/home/airflow/gcs/data/bigquery/gold.sql"
 
 # Read SQL query from file
 def read_sql_file(file_path):
